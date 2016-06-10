52f09d5290c09a5a5f7c5272dfa9ca2c8b6b1ec8
unknown <>
2016-06-10 09:52:05 -0300

    Ditched HTMLElem method as it flashes. Back to URL method

--- app.ts
+++ app.ts
@@ -1,31 +1,23 @@
 ﻿// TODO:
 //  add the flexibility of reading play_interval from site API: http://api.novascotiawebcams.com/api/image_profile/ferryterminal
 
-// Structure to hold an image.
-interface Image {
-    elem: HTMLElement;
-    url: string;
-    timestamp: number;
-}
-
 
 class ImagePlayer {
 
     // Some static configures
     static urlAPIbase: string = 'http://api.novascotiawebcams.com/api/image_profile/';
-    static buffer_seconds: number = 10;     // 10 seconds of buffer.
+    static buffer_seconds: number = 15;     // seconds of buffer.
     static play_interval: number = 2;       // 2 seconds per frame.
-    static source_delay: number = 70;      // image source appears to have ~ 60 seconds delay from real time
+    static source_delay: number = 30;      // image source appears to have ~ 30 seconds delay from real time
 
     parentElem: HTMLElement;
     childElem: HTMLElement;
 
-    apiUrl: string;
+    apiUrlBase: string;
     timerRefreshImage: number = 0;
 
-    imgBuffer: Array<HTMLElement> = [];
+    urls: Array<string> = [];
     last_fetch_timestamp: number = 0;
-
 
 
     constructor(parent: HTMLElement, camera_name: string) {
@@ -35,14 +27,15 @@ class ImagePlayer {
 
         // samle URL below:
         // http://api.novascotiawebcams.com/api/image_profile/ferryterminal/images
-        this.apiUrl = ImagePlayer.urlAPIbase + camera_name + '/images';
+        this.apiUrlBase = ImagePlayer.urlAPIbase + camera_name + '/images';
     }
 
     start() {
         // Fetching from current time minus the delay in source, minus the length of buffer.
-        this.last_fetch_timestamp = new Date().getTime() / 1000;
-        this.fetchImages(this.last_fetch_timestamp - ImagePlayer.source_delay - ImagePlayer.buffer_seconds, true); // true to kick off refreshing
-        console.log('fetching from ' + (this.last_fetch_timestamp - ImagePlayer.buffer_seconds) + ' with current timestamp:' + this.last_fetch_timestamp);
+        //this.last_fetch_timestamp = new Date().getTime() / 1000;
+        //this.fetchImages(this.last_fetch_timestamp - ImagePlayer.source_delay - ImagePlayer.buffer_seconds, true); // true to kick off refreshing
+        //console.log('fetching from ' + (this.last_fetch_timestamp - ImagePlayer.buffer_seconds) + ' with current timestamp:' + this.last_fetch_timestamp);
+        this.fetchImages('Start timers');
     }
 
     stop() {
@@ -50,22 +43,26 @@ class ImagePlayer {
     }
 
     refreshImage() {
-        console.log('Refreshing, ' + this.imgBuffer.length + ' in buffer');
-        var newChildElem: HTMLElement = this.imgBuffer.shift();
-        this.parentElem.replaceChild(newChildElem, this.childElem);
-        this.childElem = newChildElem;
+        console.log('Refreshing, ' + this.urls.length + ' in buffer');
+        this.childElem.setAttribute('src', this.urls.shift());
 
         // If we are running close to our buffer, fetch again.
-        if (this.imgBuffer.length <= (ImagePlayer.buffer_seconds / ImagePlayer.play_interval)) {
-            this.fetchImages(this.last_fetch_timestamp, false);
+        if (this.urls.length <= (ImagePlayer.buffer_seconds / ImagePlayer.play_interval)) {
+            this.fetchImages();
         }
     }
 
-    fetchImages(startingTimestamp: number, startTimers?: boolean) {
+    fetchImages(startTimers?: string) {
 
         // Sample URL below.
         // http://api.novascotiawebcams.com/api/image_profile/ferryterminal/images?absolute_timestamp=1465262275&period=30&speed=1&thumbnail=0
-        var apiUrl: string = this.apiUrl + '?absolute_timestamp=' + startingTimestamp.toString();
+        // http://api.novascotiawebcams.com/api/image_profile/ferryterminal/images?relative_timestamp=30&period=30&speed=1&thumbnail=0
+        var apiUrl: string;
+        if (this.last_fetch_timestamp === 0) {
+            apiUrl = this.apiUrlBase + '?relative_timestamp=' + ImagePlayer.source_delay;
+        } else {
+            apiUrl = this.apiUrlBase + '?absolute_timestamp=' + this.last_fetch_timestamp;
+        }
 
         console.log('Fetching ' + apiUrl);
         var self = this;
@@ -77,25 +74,18 @@ class ImagePlayer {
                 $.each(result, function (index, images) {
                     console.log('Fetched ' + images.length);
                     $.each(images, function (idx, img) {
-                        console.log('index ' + idx + ' for ' + JSON.stringify(img));
-                        // Parse the json result
-                        var url: string = img.url;
-                        var timestamp: number = img.timestamp;
+                        // Parse the json result and populate the URLs
+                        console.log('index ' + idx + ' for ' + JSON.stringify(img));    
+                        self.urls.push(img.url);
 
-                        var elem: HTMLElement = document.createElement('img');
-                        elem.setAttribute('src', url);
-                        
-                        console.log(elem);
-                        self.imgBuffer.push(elem);
-
-                        //// Update with serverside timestamp
+                        // Update with serverside timestamp
                         self.last_fetch_timestamp = img.timestamp;
                     })
                 });
-                console.log('size of current urls:' + self.imgBuffer.length.toString());
+                console.log('size of current urls:' + self.urls.length.toString());
 
                 // Start timers if requested
-                if (startTimers) {
+                if (startTimers==='Start timers') {
                     // Start timers on success
                     self.timerRefreshImage = setInterval(() => self.refreshImage(), ImagePlayer.play_interval * 1000);
                     console.log('refreshing started');
